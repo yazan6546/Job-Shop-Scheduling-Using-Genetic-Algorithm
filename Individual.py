c23cd673386f94681e5711865ae52ce0e6ad177b
yazan AbuAloun <1210145@student.birzeit.edu>
2024-05-18 16:07:03 +0300

    Added gantt chart list

--- Individual.py
+++ Individual.py
@@ -11,54 +11,6 @@ class Individual:
     def __init__(self, chromosome):
         self.chromosome = chromosome
 
-    @classmethod
-    def mutated_genes(self):
-        """
-        create random genes for mutation
-        """
-        global GENES
-        gene = random.choice(GENES)
-        return gene
-
-    @classmethod
-    def create_gnome(self):
-        """
-        create chromosome or string of genes
-        """
-        global TARGET
-        gnome_len = len(TARGET)
-        return [self.mutated_genes() for _ in range(gnome_len)]
-
-    def mate(self, par2):
-        """
-        Perform mating and produce new offspring
-        """
-
-        # chromosome for offspring
-        child_chromosome = []
-        for gp1, gp2 in zip(self.chromosome, par2.chromosome):
-
-            # random probability
-            prob = random.random()
-
-            # if prob is less than 0.45, insert gene
-            # from parent 1
-            if prob < 0.45:
-                child_chromosome.append(gp1)
-
-                # if prob is between 0.45 and 0.90, insert
-            # gene from parent 2
-            elif prob < 0.90:
-                child_chromosome.append(gp2)
-
-                # otherwise insert random gene(mutate),
-            # for maintaining diversity
-            else:
-                child_chromosome.append(self.mutated_genes())
-
-                # create new Individual(offspring) using
-        # generated chromosome for offspring
-        return Individual(child_chromosome)
 
     def calculate_makespan(self, job_dict):
         # Initialize machine availability and job completion arrays
